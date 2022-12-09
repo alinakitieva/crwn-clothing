44feedfee582bbe9d51c542553d33517906571b8
Britney Spears <kitievalina@gmail.com>
2022-12-09 21:24:21 +0300

    Add routes: 'home' and 'navigation' components

--- src/components/categories/categories.component.jsx
+++ src/components/categories/categories.component.jsx
@@ -1,7 +1,7 @@
-import './categories.styles.scss';
+import "./categories.styles.scss";
 import CategoryItem from "../category-item/category-item.component";
 
-const Categories = ({categories}) => {
+const Categories = ({ categories }) => {
 	return (
 		<div className="categories-container">
 			{categories.map((category) => (
