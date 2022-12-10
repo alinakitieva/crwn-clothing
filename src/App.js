891cbc41d15f27e59465c4ed4df02dd8c2752617
Britney Spears <kitievalina@gmail.com>
2022-12-10 14:21:44 +0300

    Add sign-in component, firebase utils and brought in firebase libraries

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Navigation />}>
 				<Route index element={<Home />} />
-				
-				
-				{/* <Route path="shop" component={component} /> */}
+				<Route path="sign-in" element={<SignIn />} />
+
+				{/* <Route path="shop" element={component} /> */}
 			</Route>
 		</Routes>
 	);
