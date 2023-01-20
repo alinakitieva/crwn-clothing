25fa01e34fdacc76aa27257a8ce9bab313af6f7e
Britney Spears <kitievalina@gmail.com>
2023-01-20 20:38:52 +0300

    Add redux-thunk to 'categories.reducer'

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
@@ -9,14 +10,16 @@ import { rootReducer } from "./root-reducer";
 const persistConfig = {
 	key: "root",
 	storage,
-	blacklist: ["user"],
+	blacklist: ["cart"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
 	reducer: persistedReducer,
-	middleware: [process.env.NODE_ENV === "development" && logger].filter(Boolean),
+	middleware: [process.env.NODE_ENV === "development" && logger, thunk].filter(
+		Boolean
+	),
 });
 
 export const persistor = persistStore(store);
