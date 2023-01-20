671c5c663726855b272232d91a6dbf6f0bcf6f12
Britney Spears <kitievalina@gmail.com>
2023-01-20 20:39:54 +0300

    Add spinner component to category and categories-preview

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,21 +1,30 @@
 import { Fragment } from "react";
 
+import Spinner from "../../components/spinner/spinner.component";
 import { useSelector } from "react-redux";
 import CategoryPreview from "../../components/category-preview/category-preview.component";
-import { selectCategoriesMap } from "../../store/categories/categories.selector";
+import {
+	selectCategoriesMap,
+	selectCategoriesIsLoading,
+} from "../../store/categories/categories.selector";
 
 const CategoriesPreview = () => {
 	const categoriesMap = useSelector(selectCategoriesMap);
+	const isLoading = useSelector(selectCategoriesIsLoading);
 
 	return (
 		<Fragment>
-			{Object.keys(categoriesMap).map((title) => (
-				<CategoryPreview
-					key={title}
-					title={title}
-					products={categoriesMap[title]}
-				/>
-			))}
+			{isLoading ? (
+				<Spinner />
+			) : (
+				Object.keys(categoriesMap).map((title) => (
+					<CategoryPreview
+						key={title}
+						title={title}
+						products={categoriesMap[title]}
+					/>
+				))
+			)}
 		</Fragment>
 	);
 };
